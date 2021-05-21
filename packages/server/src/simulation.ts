178521d3ed996e744088ac77ae32726b09ac2574
Paul Cowan <paul.cowan@cutting.scot>
2021-05-21 16:01:18 +0100

    fix store problems in handler

--- packages/server/src/simulation.ts
+++ packages/server/src/simulation.ts
@@ -13,7 +13,6 @@ export function simulation(simulators: Record<string, Simulator>): Effect<Simula
       let simulatorName = slice.get().simulator;
       let simulator = simulators[simulatorName];
       assert(simulator, `unknown simulator ${simulatorName}`);
-      let store = slice.slice("store");
       let options = slice.get().options;
 
       let behaviors = simulator(slice, options);
